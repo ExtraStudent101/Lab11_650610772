56b0522571e5bf8761507472b59661a72a7754c0
Theerapat Lumtan <theerapat_lum@cmu.ac.th>
2023-01-03 16:13:05 +0700

    Lab11_3 finished

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int N = 0;
+    float sum = 0,X=0,Z;
+    string textline;
+    ifstream score ("score.txt");
+     while(getline(score,textline))
+     {
+        Z = atof(textline.c_str()) ;
+        sum += atof(textline.c_str());
+        N++;
+        X +=  pow(Z,2);
+     }
+
+    cout << "Number of data = " << N << "\n";
+    cout << setprecision(3);
+    float mean = sum/N ;
+    cout << "Mean = " << mean << "\n" ;
+    cout << "Standard deviation = " << sqrt(X/N-pow(mean,2));
+}
